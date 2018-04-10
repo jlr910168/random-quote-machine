35879386dca974a00326c1242f0b9063fecf6ca8
Pavel <jlr910168@gmail.com>
2018-04-10 10:52:18 +0300

    basic random quote implementation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-import hello from './components/hello';
 import './style.css';
+import quotes from './quotes.json';
 
-document.body.appendChild(hello('Hello Babel'));
+const newQuote = document.getElementById('new-quote');
+const quoteText = document.getElementById('text');
+const quoteAuthor = document.getElementById('author');
+
+const randomQuote = () => {
+  const rand = max => Math.floor(Math.random() * max);
+  return quotes[rand(quotes.length)];
+};
+
+const getQuote = () => {
+  const { text, author } = randomQuote();
+  quoteText.innerHTML = text;
+  quoteAuthor.innerHTML = author;
+};
+
+window.addEventListener('load', getQuote);
+newQuote.addEventListener('click', getQuote);
