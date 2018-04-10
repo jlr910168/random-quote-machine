d67562b615a316ff4afaeca8ed4460ef0fd6b402
Pavel <jlr910168@gmail.com>
2018-04-10 14:34:09 +0300

    more styling

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import quotes from './quotes.json';
 const newQuote = document.getElementById('new-quote');
 const quoteText = document.getElementById('text');
 const quoteAuthor = document.getElementById('author');
+const tweetQuote = document.getElementById('tweet-quote');
 
 const randomQuote = () => {
   const rand = max => Math.floor(Math.random() * max);
@@ -14,6 +15,8 @@ const getQuote = () => {
   const { text, author } = randomQuote();
   quoteText.innerHTML = text;
   quoteAuthor.innerHTML = author;
+  tweetQuote.href =
+    `http://twitter.com/intent/tweet?text=${text}%0A%20%2D%20${author}`;
 };
 
 window.addEventListener('load', getQuote);
